fac146547b03e618c90ae34ce1be836a5263c732
huudo1521 <huudo1521@gmail.com>
2024-06-21 09:55:21 +0700

    Done

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.status(234).send("Welcome")
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
